55a0dbf58b5dbcbfd13783a50d97afa8cbc1b9e1
moljac <mcvjetko@holisticware.net>
2025-05-06 08:43:45 +0200

    few fixes

--- source/HolisticWare.DataScience.ML.AI.LMs.RAG.ETL.Docling/docling/convert.py
+++ source/HolisticWare.DataScience.ML.AI.LMs.RAG.ETL.Docling/docling/convert.py
@@ -8,22 +8,22 @@
 from docling.document_converter import DocumentConverter
 
 # document per local path or URL
-source = "/Volumes/FAT_VERB/learning/topics/security/threat-modelling/effectivethreatinvestigationforsocanalysts.pdf"
-# "https://arxiv.org/pdf/2408.09869"  
-# /Volumes/FAT_VERB/learning/topics/security/threat-modelling/threatmodeling.pdf
+source = "/Volumes/xFAT-1TB-2/books/security/threat-modelling/aligningsecurityoperationswiththemitreattandckframework.pdf"
+        # "/Volumes/FAT_VERB/learning/topics/security/threat-modelling/threatmodeling.pdf"
+        # "/Volumes/FAT_VERB/learning/topics/security/threat-modelling/effectivethreatinvestigationforsocanalysts.pdf"
+        # "https://arxiv.org/pdf/2408.09869"  
 
-def covert_docling(source: str) -> str:
+def covert_document_export_to_markdown(source: str) -> str:
     converter = DocumentConverter()
     print(f"python: docling.document_converter load {source}")
     result = converter.convert(source)
     resuld_md = result.document.export_to_markdown()  # output: "## Docling Technical Report[...]"
-    print(resuld_md)
+    # print(resuld_md)
     # save to file
-    print(f"python: docling.document_converter save {source}.docling.md")
-    with open(f"{source}.docling.md", "w") as f:
-        f.write(result_md) 
+    # print(f"python: docling.document_converter save {source}.docling.md")
+    # with open(f"{source}.docling.md", "w") as f:
+    #    f.write(result_md) 
 
     return result_md
 
-
-covert_docling(source)
+covert_document_export_to_markdown(source)
